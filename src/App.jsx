3a2aa12d65932bdddc88c7ec5f166111ba7a72cf
Rayahalwani03 <mira.res.mc@example.com>
2025-01-02 16:47:52 +0300

    Routers stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-export default function App() {
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import ForgotPassword from './pages/ForgotPassword'
+import Home from './pages/Home'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-400">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   )
 }
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div>
+      <h1 className="bg-red-400">home</h1>
+    </div>
+  )
+}
+
+export default Home
